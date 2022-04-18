17e3b69af200140d44392731ec61d2b4e368f926
Eduardo <emartinezp1403@DESKTOP-1UTMGOV>
2022-04-17 20:20:57 -0500

    Esta es una configuración que sí funciona.

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
-
 #include <xc.h>
+#include "prototipos.h"
 //#include "pic16f886.inc"
 
 void main(void) {
+  //  setup();
+    TRISA = 0b00001111;
+    TRISB = 0x00;
+
+    unsigned Encoder1 = 0x04;
+    unsigned Encoder2 = 0x05;
+    
+    motor();
     
     return;
 }
